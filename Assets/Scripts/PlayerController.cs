e2e69f6e12b242084a65dea6536270e956c49987
JPMeeks <meeks.preston1@gmail.com>
2018-11-21 11:13:50 -0500

    Added some lights

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,65 +5,58 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	// variables
-	public GameObject ironStakePrefab;	
-	public GameObject woodStakePrefab;	
-	public GameObject meatStakePrefab;
+        // variables
+        private GameObject light;
 
-	public GameObject ironStakeUIPrefab;
-	public GameObject woodStakeUIPrefab;
-	public GameObject meatStakeUIPrefab;
+	public GameObject lightPrefab;
+
+	public GameObject lightUIPrefab;
 	
-	public Transform stakeSpawn;
+	public Transform lightSpawn;
 	public GameObject playerCharacter;
 	
 	public GameObject heldItemParent;
-	public GameObject heldStake;
-	public GameObject heldStakeUIParent;
-	public GameObject heldStakeUI;
-	public Text heldStakeText;
+	public GameObject heldLight;
+	public GameObject heldLightUIParent;
+	public GameObject heldLightUI;
+	public Text heldLightText;
 	public Text scoreText;
 
-    public GameObject healthSlider;
+        public GameObject healthSlider;
 	
 	public GameObject SceneManager;
 	public string stateString;
 
 	private float lastFire;
 
-	public enum Weapons 
-	{
-	    Iron = 1,
-	    Wood = 2,
-	    Meat = 3
-	};
-	public Weapons currentWeapon;
-	private int numWeapons;
+        public bool sticky = true;
+
+
 	public int health;
+        public int ammo;
 	public int score;
 
-	private float ironFireTime = 0.5f;
-	private float woodFireTime = 0.5f;
-	private float meatFireTime = 0.5f;
+	private float fireTime = 0.5f;
 
 	void Start ()
 	{
-	    stateString = SceneManager.GetComponent<MainSceneManager>().currentState.ToString();
-	    numWeapons = System.Enum.GetNames(typeof(Weapons)).Length;
-	    currentWeapon = Weapons.Iron;
-        healthSlider = GameObject.FindGameObjectWithTag("HealthSlider");
-        health = 100;
+	        stateString = SceneManager.GetComponent<MainSceneManager>().currentState.ToString();
+
+                healthSlider = GameObject.FindGameObjectWithTag("HealthSlider");
+                health = 100;
 		
 		lastFire = 0;
+
+                ammo = 5;
 	}
 	
 	void Update () 
 	{
-	    stateString = SceneManager.GetComponent<MainSceneManager>().currentState.ToString();
-        scoreText.text = "Score: " + score;
+	        stateString = SceneManager.GetComponent<MainSceneManager>().currentState.ToString();
+                //scoreText.text = "Score: " + score;
 
-	    if (stateString == "Ingame")
-	    {
+	        if (stateString == "Ingame")
+	        {
 	        float xPos = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 	        float yPos = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
 	        float zPos = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
@@ -71,159 +64,127 @@ public class PlayerController : MonoBehaviour
 	        transform.Rotate(0, xPos, 0);
 	        transform.Translate(0, 0, zPos);
 
-            // Shooting Input - Left Click
-            if (Input.GetMouseButton(0))
-	        {
-	            switch (currentWeapon)
-	            {
-	                case Weapons.Iron:
-				if(Time.time > ironFireTime + lastFire)
-					Fire(ironStakePrefab);
-				break;
-	                case Weapons.Wood:
-				if (Time.time > woodFireTime + lastFire)
-					Fire(woodStakePrefab);
-				break;
-	                case Weapons.Meat:
-				if (Time.time > meatFireTime + lastFire)
-					Fire(meatStakePrefab);
-				break;
-	                default:
-				break;
-	            }
-	        }
+                        // Shooting Input - Left Click
+                        if (Input.GetMouseButton(0))
+	                {
+		        	if(Time.time > fireTime + lastFire && ammo > 0)
+                                {
+                                        Fire(lightPrefab);
+                                        ammo--;
+                                }
+	                }
 	
-	        // Cycle weapons on scroll
-	        if (Input.mouseScrollDelta.y > 0f)
-	            CycleWeapons(true);
-	        else if (Input.mouseScrollDelta.y < 0f)
-	            CycleWeapons(false);
+	        //// Cycle weapons on scroll
+	        //if (Input.mouseScrollDelta.y > 0f)
+	        //    CycleWeapons(true);
+	        //else if (Input.mouseScrollDelta.y < 0f)
+	        //    CycleWeapons(false);
 	    }
 	}
 
-	private void Fire(GameObject stakePrefab)
+	private void Fire(GameObject lightPrefab)
 	{
-		// create a transform from the stakeSpawn and the playerCharacter
-		Transform stakeRot = stakeSpawn;
-		stakeRot.rotation = playerCharacter.transform.rotation;
+		// create a transform from the lightSpawn and the playerCharacter
+		Transform lightRot = lightSpawn;
+		lightRot.rotation = playerCharacter.transform.rotation;
 	
-		// create a stake from a bullet prefab
-		var stake = (GameObject)Instantiate(stakePrefab, stakeSpawn.position, stakeRot.rotation);
+		// create a light from a bullet prefab
+	        light = (GameObject)Instantiate(lightPrefab, lightSpawn.position, lightRot.rotation);
 	
-		// add velocity to the stake
-		stake.GetComponent<Rigidbody>().velocity = stake.transform.forward * 20;
+		// add velocity to the light
+		light.GetComponent<Rigidbody>().velocity = light.transform.forward * 10;
 
-		// mark the time the stake was created
+		// mark the time the light was created
 		lastFire = Time.time;
 
-		// remove stake after 2 seconds
-		Destroy(stake, 2.0f);
+		// remove light after 2 seconds
+		Destroy(light, 5.0f);
 	}
 	
+        /*
 	void CycleWeapons(bool forward)
 	{
-	    int currentIndex = (int)currentWeapon;
-	    int increment = (forward) ? 1 : -1;
-	
-	    currentIndex += increment;
-	
-	    // Fix out of bounds
-	    if (currentIndex < 1)
-	        currentIndex = numWeapons;
-	    else if (currentIndex > numWeapons)
-	        currentIndex = 1;
-	
-	    currentWeapon = (Weapons)currentIndex;
-	    UpdateWeaponViews();
+	        int currentIndex = (int)currentWeapon;
+	        int increment = (forward) ? 1 : -1;
+	        
+	        currentIndex += increment;
+	        
+	        // Fix out of bounds
+	        if (currentIndex < 1)
+	            currentIndex = numWeapons;
+	        else if (currentIndex > numWeapons)
+	            currentIndex = 1;
+	        
+	        currentWeapon = (Weapons)currentIndex;
+	        UpdateWeaponViews();
 	}
+        */
 	
 	void UpdateWeaponViews()
 	{
-	    Transform prevStakeTransform = heldStake.transform;
-	    Transform uiStakeTransform = heldStakeUI.transform;
-	    Destroy(heldStake);
-	    Destroy(heldStakeUI);
-	
-	    switch (currentWeapon)
-	    {
-	        case Weapons.Iron:
-	            // Update held item stake
-	            GameObject newIronStake = (GameObject)Instantiate(ironStakePrefab, prevStakeTransform.position, prevStakeTransform.rotation);
-                newIronStake.gameObject.GetComponentInChildren<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-                Destroy(newIronStake.GetComponent<Rigidbody>());
-	            newIronStake.transform.parent = heldItemParent.transform;
-                heldStake = newIronStake;
+	        Transform prevlightTransform = heldLight.transform;
+	        Transform uilightTransform = heldLightUI.transform;
+	        Destroy(heldLight);
+	        Destroy(heldLightUI);
+
+	        // Update held item light
+	        GameObject newLight = (GameObject)Instantiate(lightPrefab, prevlightTransform.position, prevlightTransform.rotation);
+                newLight.gameObject.GetComponentInChildren<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                Destroy(newLight.GetComponent<Rigidbody>());
+	        newLight.transform.parent = heldItemParent.transform;
+                heldLight = newLight;
 
 
                 //Update UI Text
-                heldStakeText.text = "Iron Stakes (Zombies)";
-	
-	            //Update UI Stake
-	            GameObject newIronStakeUI = (GameObject)Instantiate(ironStakeUIPrefab, uiStakeTransform.localPosition, uiStakeTransform.localRotation);
-	            newIronStakeUI.transform.SetParent(heldStakeUIParent.transform, false);
-	            heldStakeUI = newIronStakeUI;
-	            break;
-	        case Weapons.Wood:
-	            GameObject newWoodStake = (GameObject)Instantiate(woodStakePrefab, prevStakeTransform.position, prevStakeTransform.rotation);
-                newWoodStake.gameObject.GetComponentInChildren<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-                Destroy(newWoodStake.GetComponent<Rigidbody>());
-	            newWoodStake.transform.parent = heldItemParent.transform;
-                heldStake = newWoodStake;
-
-                heldStakeText.text = "Wood Stakes (Vampires)";
-	
-	            GameObject newWoodStakeUI = (GameObject)Instantiate(woodStakeUIPrefab, uiStakeTransform.localPosition, uiStakeTransform.localRotation);
-	            newWoodStakeUI.transform.SetParent(heldStakeUIParent.transform, false);
-	            heldStakeUI = newWoodStakeUI;
-	            break;
-	        case Weapons.Meat:
-	            GameObject newMeatStake = (GameObject)Instantiate(meatStakePrefab, prevStakeTransform.position, prevStakeTransform.rotation);
-                newMeatStake.gameObject.GetComponentInChildren<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-                Destroy(newMeatStake.GetComponent<Rigidbody>());
-	            newMeatStake.transform.parent = heldItemParent.transform;
-                heldStake = newMeatStake;
-
-                heldStakeText.text = "Meat Steaks (Ghosts)";
-	
-	            GameObject newMeatStakeUI = (GameObject)Instantiate(meatStakeUIPrefab, uiStakeTransform.localPosition, uiStakeTransform.localRotation);
-	            newMeatStakeUI.transform.SetParent(heldStakeUIParent.transform, false);
-	            heldStakeUI = newMeatStakeUI;
-	
-	            break;
-	        default:
-	            break;
-	    }
+                heldLightText.text = "Light";
+	        
+	        //Update UI light
+	        GameObject newLightUI = (GameObject)Instantiate(lightUIPrefab, uilightTransform.localPosition, uilightTransform.localRotation);
+	        newLightUI.transform.SetParent(heldLightUIParent.transform, false);
+	        heldLightUI = newLightUI;
 	}
 	
 	private void OnTriggerEnter(Collider other)
 	{
-	    if (stateString == "Ingame")
-	    {
-	        // Kill player on collision with water
-	        if (other.gameObject.name == "Water4Advanced")
-	        {
-	            health = 0;
-	        }
+                if (stateString == "Ingame")
+                {
+                        // Kill player on collision with water
+                        if (other.gameObject.name == "Water4Advanced")
+                        {
+                            health = 0;
+                        }
 
-            // decrement player health when a ghost or vampire is "hitting" them
-            if (other.gameObject.name == "Ghost" || other.gameObject.name == "Ghost(Clone)" || other.gameObject.name == "Vampire" || other.gameObject.name == "Vampire(Clone)")
-            {
-                health--;
-                healthSlider.GetComponent<Slider>().value = health;
-            }
-        }
+                        //// stick lights to surfaces
+                        //if (other.gameObject.tag == "Iron" && other.gameObject.tag != "Player" && sticky)
+                        //{
+                        //        light.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                        //}
+                
+                        // decrement player health when a ghost or vampire is "hitting" them
+                        if (other.gameObject.name == "Ghost" || other.gameObject.name == "Ghost(Clone)" || other.gameObject.name == "Vampire" || other.gameObject.name == "Vampire(Clone)")
+                        {
+                            health--;
+                            healthSlider.GetComponent<Slider>().value = health;
+                        }
+
+                        if(other.gameObject.tag == "Pickup")
+                        {
+                                ammo++;
+                                Destroy(other.gameObject, 0);
+                        }
+                }
 	}
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (stateString == "Ingame")
+        private void OnTriggerStay(Collider other)
         {
-            // decrement player health when a ghost or vampire is "hitting" them
-            if (other.gameObject.name == "Ghost" || other.gameObject.name == "Ghost(Clone)" || other.gameObject.name == "Vampire" || other.gameObject.name == "Vampire(Clone)")
+            if (stateString == "Ingame")
             {
-                health--;
-                healthSlider.GetComponent<Slider>().value = health;
+                // decrement player health when a ghost or vampire is "hitting" them
+                if (other.gameObject.name == "Ghost" || other.gameObject.name == "Ghost(Clone)" || other.gameObject.name == "Vampire" || other.gameObject.name == "Vampire(Clone)")
+                {
+                    health--;
+                    healthSlider.GetComponent<Slider>().value = health;
+                }
             }
         }
-    }
 }
